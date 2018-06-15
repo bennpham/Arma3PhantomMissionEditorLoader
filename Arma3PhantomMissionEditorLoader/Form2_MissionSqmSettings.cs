4b538b570b1844f8134ec20f56b6465292f09d82
bennpham <bennguyenpham@hotmail.com>
2018-06-14 20:45:36 -0700

    WIP handling overwriting lines for mission.sqm

--- Arma3PhantomMissionEditorLoader/Form2_MissionSqmSettings.cs
+++ Arma3PhantomMissionEditorLoader/Form2_MissionSqmSettings.cs
@@ -13,15 +13,38 @@ namespace Arma3PhantomMissionEditorLoader
 	public partial class Form2_MissionSqmSettings : Form
 	{
 		private const String MISSION_SQM_BACKUP = "mission.sqm.old";
+		private readonly String[] LIST_SCENARIO_DATA = {
+			"author", "overviewText", "overViewPicture", "onLoadMission", "loadScreen", "class Header"
+		}; 
+		private readonly String[] LIST_CUSTOM_ATTRIBUTES = {
+			"class Category", "name=\"Multiplayer\";", "property=\"RespawnTemplates\";"
+		}; 
+		private readonly String[] LIST_INTEL = {
+			"overviewText",
+			"resistanceWest", "resistanceEast",
+			"timeOfChanges",
+			"startWeather", "startFog", "forecastWeather", "forecastFog",
+			"year", "day", "hour", "minute", 
+			"startFogBase", "forecastFogBase", "startFogDecay", "forecastFogDecay"
+		}; 
 
-		public String missionSQM;
-		public String missionDirectory;
+		private String missionSQM;
+		private String missionDirectory;
+
+		// Check to see if all sections of mission.sqm is handled (ScenarioData, CustomAttributes, Intel)
+		private bool isHandledScenarioData;
+		private bool isHandledCustomAttributes;
+		private bool isHandledIntel;
 
 		public Form2_MissionSqmSettings(String missionSQM, String missionDirectory)
 		{
 			InitializeComponent();
 			this.missionSQM = missionSQM;
 			this.missionDirectory = missionDirectory;
+
+			isHandledScenarioData = false;
+			isHandledCustomAttributes = false;
+			isHandledIntel = false;
 		}
 
 		private void missionsqm_button_Click(object sender, EventArgs e)
@@ -37,24 +60,50 @@ namespace Arma3PhantomMissionEditorLoader
 			{
 				using (System.IO.StreamWriter sw = new System.IO.StreamWriter(this.missionSQM))
 				{
+					bool editingScenarioData = false;
+					bool editingCustomAttributes = false;
+					bool editingIntel = false;
+
 					while ((line = sr.ReadLine()) != null)
 					{
-						// TODO IF ELSE STATEMENT 
-						if (line.Contains("TODO"))
+						if (editingScenarioData || editingCustomAttributes || editingIntel)
 						{
-							// TODO
+							sw.WriteLine(line); // Writes open bracket to mission.sqm
+
+							while (editingScenarioData && (line = sr.ReadLine()) != null)
+							{
+								isHandledScenarioData = true;
+							}
+							while (editingCustomAttributes && (line = sr.ReadLine()) != null)
+							{
+								isHandledCustomAttributes = true;
+							}
+							while (editingIntel && (line = sr.ReadLine()) != null)
+							{
+								isHandledIntel = true;
+							}
 						}
-						else
+						else if (line.Contains("class ScenarioData"))
 						{
-							// TODO
+							editingScenarioData = true;
 						}
+						else if (line.Contains("class CustomAttributes"))
+						{
+							editingCustomAttributes = true;
+						}
+						else if (line.Contains("	class Intel"))
+						{
+							editingIntel = true;
+						}
+
+						sw.WriteLine(line);	
 					}
 				}
 			}
 
 			// GoTo Generate infotext Form
 			//		Pick Name of Mission to Display (already have date and Author)
-
+			Environment.Exit(0); // TODO placeholder
 		}
 	}
 }
