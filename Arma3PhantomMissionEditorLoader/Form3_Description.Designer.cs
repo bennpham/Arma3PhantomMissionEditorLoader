5d7f113b07950c0f82c5aeaf5166c4c20bf9b097
bennpham <bennguyenpham@hotmail.com>
2018-08-11 19:48:34 -0700

    Added Zeus Init Checkbox

--- Arma3PhantomMissionEditorLoader/Form3_Description.Designer.cs
+++ Arma3PhantomMissionEditorLoader/Form3_Description.Designer.cs
@@ -36,6 +36,8 @@
 			this.infotext_title = new System.Windows.Forms.TextBox();
 			this.label_created_by = new System.Windows.Forms.Label();
 			this.label_datetime = new System.Windows.Forms.Label();
+			this.init_zeus_checkbox = new System.Windows.Forms.CheckBox();
+			this.init_zeus_label = new System.Windows.Forms.Label();
 			this.SuspendLayout();
 			// 
 			// title_description
@@ -76,7 +78,7 @@
 			// description_button
 			// 
 			this.description_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.description_button.Location = new System.Drawing.Point(176, 331);
+			this.description_button.Location = new System.Drawing.Point(176, 376);
 			this.description_button.Name = "description_button";
 			this.description_button.Size = new System.Drawing.Size(147, 38);
 			this.description_button.TabIndex = 13;
@@ -88,7 +90,7 @@
 			// 
 			this.title_infotext.AutoSize = true;
 			this.title_infotext.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.title_infotext.Location = new System.Drawing.Point(12, 127);
+			this.title_infotext.Location = new System.Drawing.Point(12, 172);
 			this.title_infotext.Name = "title_infotext";
 			this.title_infotext.Size = new System.Drawing.Size(174, 29);
 			this.title_infotext.TabIndex = 14;
@@ -97,7 +99,7 @@
 			// 
 			// infotext_title
 			// 
-			this.infotext_title.Location = new System.Drawing.Point(12, 203);
+			this.infotext_title.Location = new System.Drawing.Point(12, 248);
 			this.infotext_title.Multiline = true;
 			this.infotext_title.Name = "infotext_title";
 			this.infotext_title.Size = new System.Drawing.Size(440, 75);
@@ -107,7 +109,7 @@
 			// 
 			this.label_created_by.AutoSize = true;
 			this.label_created_by.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label_created_by.Location = new System.Drawing.Point(13, 290);
+			this.label_created_by.Location = new System.Drawing.Point(13, 335);
 			this.label_created_by.Name = "label_created_by";
 			this.label_created_by.Size = new System.Drawing.Size(174, 20);
 			this.label_created_by.TabIndex = 22;
@@ -117,17 +119,42 @@
 			// 
 			this.label_datetime.AutoSize = true;
 			this.label_datetime.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label_datetime.Location = new System.Drawing.Point(12, 170);
+			this.label_datetime.Location = new System.Drawing.Point(12, 215);
 			this.label_datetime.Name = "label_datetime";
 			this.label_datetime.Size = new System.Drawing.Size(199, 20);
 			this.label_datetime.TabIndex = 23;
 			this.label_datetime.Text = "June 1, 2018 | 12:00:00";
 			// 
+			// init_zeus_checkbox
+			// 
+			this.init_zeus_checkbox.AutoSize = true;
+			this.init_zeus_checkbox.Checked = true;
+			this.init_zeus_checkbox.CheckState = System.Windows.Forms.CheckState.Checked;
+			this.init_zeus_checkbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.init_zeus_checkbox.Location = new System.Drawing.Point(17, 112);
+			this.init_zeus_checkbox.Name = "init_zeus_checkbox";
+			this.init_zeus_checkbox.Size = new System.Drawing.Size(306, 24);
+			this.init_zeus_checkbox.TabIndex = 24;
+			this.init_zeus_checkbox.Text = "Init Zeus (Needs 3 Playable Zeus Slots)";
+			this.init_zeus_checkbox.UseVisualStyleBackColor = true;
+			// 
+			// init_zeus_label
+			// 
+			this.init_zeus_label.AutoSize = true;
+			this.init_zeus_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.init_zeus_label.Location = new System.Drawing.Point(12, 139);
+			this.init_zeus_label.Name = "init_zeus_label";
+			this.init_zeus_label.Size = new System.Drawing.Size(427, 17);
+			this.init_zeus_label.TabIndex = 25;
+			this.init_zeus_label.Text = "Need to name the zeus slots zeus_mod1, zeus_mod2, zeus_mod3";
+			// 
 			// Form3_Description
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(464, 381);
+			this.ClientSize = new System.Drawing.Size(464, 421);
+			this.Controls.Add(this.init_zeus_label);
+			this.Controls.Add(this.init_zeus_checkbox);
 			this.Controls.Add(this.label_datetime);
 			this.Controls.Add(this.label_created_by);
 			this.Controls.Add(this.infotext_title);
@@ -155,5 +182,7 @@
 		private System.Windows.Forms.TextBox infotext_title;
 		private System.Windows.Forms.Label label_created_by;
 		private System.Windows.Forms.Label label_datetime;
+		private System.Windows.Forms.CheckBox init_zeus_checkbox;
+		private System.Windows.Forms.Label init_zeus_label;
 	}
 }
