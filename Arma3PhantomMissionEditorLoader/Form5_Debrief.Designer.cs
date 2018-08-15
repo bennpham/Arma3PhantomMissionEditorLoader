e139fdbcd3f01b500c424d7fcd4fb16e7434eb16
bennpham <bennguyenpham@hotmail.com>
2018-08-14 23:33:46 -0700

    Completed UI For Form5 Debriefing

---
+++ Arma3PhantomMissionEditorLoader/Form5_Debrief.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace Arma3PhantomMissionEditorLoader
+{
+	partial class Form5_Debrief
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.title_debriefing = new System.Windows.Forms.Label();
+			this.classNameLabel = new System.Windows.Forms.Label();
+			this.textbox_classname = new System.Windows.Forms.TextBox();
+			this.titleLabel = new System.Windows.Forms.Label();
+			this.textbox_title = new System.Windows.Forms.TextBox();
+			this.button_win = new System.Windows.Forms.Button();
+			this.button_lose = new System.Windows.Forms.Button();
+			this.subtitleLabel = new System.Windows.Forms.Label();
+			this.textbox_subtitle = new System.Windows.Forms.TextBox();
+			this.descriptionLabel = new System.Windows.Forms.Label();
+			this.textbox_description = new System.Windows.Forms.TextBox();
+			this.pictureBackgroundLabel = new System.Windows.Forms.Label();
+			this.textbox_pictureBackground = new System.Windows.Forms.TextBox();
+			this.button_complete = new System.Windows.Forms.Button();
+			this.button_add = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// title_debriefing
+			// 
+			this.title_debriefing.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.title_debriefing.AutoSize = true;
+			this.title_debriefing.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.title_debriefing.Location = new System.Drawing.Point(7, 9);
+			this.title_debriefing.Name = "title_debriefing";
+			this.title_debriefing.Size = new System.Drawing.Size(135, 29);
+			this.title_debriefing.TabIndex = 4;
+			this.title_debriefing.Text = "Debriefing";
+			this.title_debriefing.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+			// 
+			// classNameLabel
+			// 
+			this.classNameLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.classNameLabel.AutoSize = true;
+			this.classNameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.classNameLabel.Location = new System.Drawing.Point(8, 48);
+			this.classNameLabel.Name = "classNameLabel";
+			this.classNameLabel.Size = new System.Drawing.Size(104, 20);
+			this.classNameLabel.TabIndex = 5;
+			this.classNameLabel.Text = "Class Name";
+			this.classNameLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+			// 
+			// textbox_classname
+			// 
+			this.textbox_classname.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.textbox_classname.Location = new System.Drawing.Point(12, 71);
+			this.textbox_classname.Name = "textbox_classname";
+			this.textbox_classname.Size = new System.Drawing.Size(539, 20);
+			this.textbox_classname.TabIndex = 8;
+			// 
+			// titleLabel
+			// 
+			this.titleLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.titleLabel.AutoSize = true;
+			this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.titleLabel.Location = new System.Drawing.Point(12, 108);
+			this.titleLabel.Name = "titleLabel";
+			this.titleLabel.Size = new System.Drawing.Size(43, 20);
+			this.titleLabel.TabIndex = 9;
+			this.titleLabel.Text = "Title";
+			this.titleLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+			// 
+			// textbox_title
+			// 
+			this.textbox_title.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.textbox_title.Location = new System.Drawing.Point(12, 131);
+			this.textbox_title.Name = "textbox_title";
+			this.textbox_title.Size = new System.Drawing.Size(680, 20);
+			this.textbox_title.TabIndex = 10;
+			// 
+			// button_win
+			// 
+			this.button_win.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.button_win.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button_win.Location = new System.Drawing.Point(558, 67);
+			this.button_win.Name = "button_win";
+			this.button_win.Size = new System.Drawing.Size(64, 24);
+			this.button_win.TabIndex = 11;
+			this.button_win.Text = "Win";
+			this.button_win.UseVisualStyleBackColor = true;
+			// 
+			// button_lose
+			// 
+			this.button_lose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.button_lose.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button_lose.Location = new System.Drawing.Point(628, 67);
+			this.button_lose.Name = "button_lose";
+			this.button_lose.Size = new System.Drawing.Size(64, 24);
+			this.button_lose.TabIndex = 12;
+			this.button_lose.Text = "Lose";
+			this.button_lose.UseVisualStyleBackColor = true;
+			// 
+			// subtitleLabel
+			// 
+			this.subtitleLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.subtitleLabel.AutoSize = true;
+			this.subtitleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.subtitleLabel.Location = new System.Drawing.Point(12, 167);
+			this.subtitleLabel.Name = "subtitleLabel";
+			this.subtitleLabel.Size = new System.Drawing.Size(71, 20);
+			this.subtitleLabel.TabIndex = 13;
+			this.subtitleLabel.Text = "Subtitle";
+			this.subtitleLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+			// 
+			// textbox_subtitle
+			// 
+			this.textbox_subtitle.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.textbox_subtitle.Location = new System.Drawing.Point(12, 190);
+			this.textbox_subtitle.Name = "textbox_subtitle";
+			this.textbox_subtitle.Size = new System.Drawing.Size(680, 20);
+			this.textbox_subtitle.TabIndex = 14;
+			// 
+			// descriptionLabel
+			// 
+			this.descriptionLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.descriptionLabel.AutoSize = true;
+			this.descriptionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.descriptionLabel.Location = new System.Drawing.Point(12, 224);
+			this.descriptionLabel.Name = "descriptionLabel";
+			this.descriptionLabel.Size = new System.Drawing.Size(100, 20);
+			this.descriptionLabel.TabIndex = 15;
+			this.descriptionLabel.Text = "Description";
+			this.descriptionLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+			// 
+			// textbox_description
+			// 
+			this.textbox_description.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.textbox_description.Location = new System.Drawing.Point(12, 247);
+			this.textbox_description.Multiline = true;
+			this.textbox_description.Name = "textbox_description";
+			this.textbox_description.Size = new System.Drawing.Size(680, 96);
+			this.textbox_description.TabIndex = 16;
+			// 
+			// pictureBackgroundLabel
+			// 
+			this.pictureBackgroundLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.pictureBackgroundLabel.AutoSize = true;
+			this.pictureBackgroundLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.pictureBackgroundLabel.Location = new System.Drawing.Point(12, 352);
+			this.pictureBackgroundLabel.Name = "pictureBackgroundLabel";
+			this.pictureBackgroundLabel.Size = new System.Drawing.Size(166, 20);
+			this.pictureBackgroundLabel.TabIndex = 17;
+			this.pictureBackgroundLabel.Text = "Picture Background";
+			this.pictureBackgroundLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+			// 
+			// textbox_pictureBackground
+			// 
+			this.textbox_pictureBackground.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.textbox_pictureBackground.Location = new System.Drawing.Point(12, 375);
+			this.textbox_pictureBackground.Name = "textbox_pictureBackground";
+			this.textbox_pictureBackground.Size = new System.Drawing.Size(680, 20);
+			this.textbox_pictureBackground.TabIndex = 18;
+			this.textbox_pictureBackground.Text = "images\\loadScreen.jpg";
+			// 
+			// button_complete
+			// 
+			this.button_complete.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+			this.button_complete.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button_complete.Location = new System.Drawing.Point(377, 421);
+			this.button_complete.Name = "button_complete";
+			this.button_complete.Size = new System.Drawing.Size(174, 40);
+			this.button_complete.TabIndex = 19;
+			this.button_complete.Text = "COMPLETE";
+			this.button_complete.UseVisualStyleBackColor = true;
+			// 
+			// button_add
+			// 
+			this.button_add.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+			this.button_add.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button_add.Location = new System.Drawing.Point(154, 421);
+			this.button_add.Name = "button_add";
+			this.button_add.Size = new System.Drawing.Size(174, 40);
+			this.button_add.TabIndex = 20;
+			this.button_add.Text = "ADD";
+			this.button_add.UseVisualStyleBackColor = true;
+			// 
+			// Form5_Debrief
+			// 
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+			this.ClientSize = new System.Drawing.Size(704, 473);
+			this.Controls.Add(this.button_add);
+			this.Controls.Add(this.button_complete);
+			this.Controls.Add(this.textbox_pictureBackground);
+			this.Controls.Add(this.pictureBackgroundLabel);
+			this.Controls.Add(this.textbox_description);
+			this.Controls.Add(this.descriptionLabel);
+			this.Controls.Add(this.textbox_subtitle);
+			this.Controls.Add(this.subtitleLabel);
+			this.Controls.Add(this.button_lose);
+			this.Controls.Add(this.button_win);
+			this.Controls.Add(this.textbox_title);
+			this.Controls.Add(this.titleLabel);
+			this.Controls.Add(this.textbox_classname);
+			this.Controls.Add(this.classNameLabel);
+			this.Controls.Add(this.title_debriefing);
+			this.Name = "Form5_Debrief";
+			this.Text = "Form5_Debrief";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label title_debriefing;
+		private System.Windows.Forms.Label classNameLabel;
+		private System.Windows.Forms.TextBox textbox_classname;
+		private System.Windows.Forms.Label titleLabel;
+		private System.Windows.Forms.TextBox textbox_title;
+		private System.Windows.Forms.Button button_win;
+		private System.Windows.Forms.Button button_lose;
+		private System.Windows.Forms.Label subtitleLabel;
+		private System.Windows.Forms.TextBox textbox_subtitle;
+		private System.Windows.Forms.Label descriptionLabel;
+		private System.Windows.Forms.TextBox textbox_description;
+		private System.Windows.Forms.Label pictureBackgroundLabel;
+		private System.Windows.Forms.TextBox textbox_pictureBackground;
+		private System.Windows.Forms.Button button_complete;
+		private System.Windows.Forms.Button button_add;
+	}
+}
